fff16745c927fc80561794adfb2d80170aab9ee3
Lev2307 <Unfortunately1366@gmail.com>
2025-09-11 13:01:17 +0300

    забыл убрать t.py ;>

--- t.py
+++
@@ -1,7 +0,0 @@
-g = [_ for _ in range(1, 101)]
-blocks_7 = [[] for _ in range((101 // 7)+1)]
-for i in range(len(g)):
-    print(i//7, g[i])
-    blocks_7[(i // 7)].append(g[i])
-print(blocks_7)
-print(len(blocks_7))
